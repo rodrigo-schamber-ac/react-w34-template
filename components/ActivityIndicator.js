c4415ff5e1e234184da8d386cf9afda78ac3dc89
Rodrigo Schamber <rodrigo.schamber@avenuecode.com>
2021-06-09 15:09:35 -0300

    [ADD] There was added an activity indicator compontent to the project.

---
+++ components/ActivityIndicator.js
@@ -0,0 +1,9 @@
+export default function ActivityIndicator(){
+    return(
+        <div className='flex'>
+            <div className="h-2.5 w-2.5 bg-current rounded-full mr-1 animate-bounce"></div>
+            <div className="h-2.5 w-2.5 bg-current rounded-full mr-1 animate-bounce200"></div>
+            <div className="h-2.5 w-2.5 bg-current rounded-full animate-bounce400"></div>
+        </div>
+    );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import ActivityIndicator from '../components/ActivityIndicator';
 import FormatDate from '../components/FormatDate';
 import NasaCard from '../components/NasaCard';
 import NasaPicture from '../components/NasaPicture';
